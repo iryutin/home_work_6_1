b2a64e89262883bb1ea73acb1b5802d9e766a13f
Ilia Ryutin <iryutin@mail.ru>
2025-04-25 11:56:56 +0300

    Настроил базу и создал модели

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Category (models.Model):
+    name_category = models.CharField(max_length= 150)
+    description = models.TextField(null=True)
+
+class Product(models.Model):
+    name = models.CharField(max_length= 150, verbose_name= "Имя_продукта")
+    description = models.TextField(null=True)
+    image = models.ImageField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.IntegerField()
+    created_at  = models.DateTimeField(auto_created=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
